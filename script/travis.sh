745f4b7e0d239f31aded873e3f1f68f7ef6a7b1b
Brendan Forster <github@brendanforster.com>
2018-02-28 18:30:35 +1100

    what even is scripting

--- script/travis.sh
+++ script/travis.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-if [[ ! -z "$LINT_ONLY" ]]; then
+if [ ${LINT_ONLY:-} ]; then
   echo "LINT_ONLY has been set - running subset of build process"
   yarn lint
-elif [[ ! -z "${BUILD_AND_TEST_ONLY}" ]]; then
+elif [ ${BUILD_AND_TEST_ONLY:-} ]; then
   echo "BUILD_AND_TEST_ONLY has been set - running subset of build process"
   yarn build:prod && yarn test:setup && yarn test
 else
