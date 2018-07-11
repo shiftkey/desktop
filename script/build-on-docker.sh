75114e3d82fcef5f7f76b73b5880499936644cc3
Brendan Forster <github@brendanforster.com>
2018-07-11 12:08:24 -0300

    pull the container rather than building each time

--- script/build-on-docker.sh
+++ script/build-on-docker.sh
@@ -3,12 +3,9 @@ ROOTDIR="$(dirname $DIRNAME)"
 ROOTPATH="$(readlink --canonicalize $ROOTDIR)"
 
 if command -v docker ; then
-  # build the custom container for Desktop
-  docker build -t desktop/linux $ROOTPATH/script/docker/linux
-
-  # mount the repository and build and test the package
   docker run --rm \
-    -v $ROOTPATH:/project desktop/linux \
+    -v $ROOTPATH:/project \
+    shiftkey/github-desktop:trusty \
     /bin/bash -c "cd /project && yarn && yarn build:prod && yarn test && yarn run package"
 else
   echo "docker not found on PATH, which is required"
