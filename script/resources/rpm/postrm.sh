fdea668ceff8c1cf59aa36a33b47cc92624ecc75
Brendan Forster <github@brendanforster.com>
2020-05-16 15:08:28 -0300

    first stab at scripts for postinst and postrm

--- script/resources/rpm/postrm.sh
+++ script/resources/rpm/postrm.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-set -e
+PROFILE_D_FILE="/etc/profile.d/github-desktop.sh"
+BASE_FILE="/usr/bin/github"
 
-# TODO: additional steps
+# cleanup profile file
+echo "#!/bin/sh" > "${PROFILE_D_FILE}";
+. "${PROFILE_D_FILE}";
+rm "${PROFILE_D_FILE}";
+
+# remove symbolic links in /usr/bin directory
+test -f ${BASE_FILE} && unlink ${BASE_FILE}
+test -f ${BASE_FILE}-desktop && unlink ${BASE_FILE}-desktop
+test -f ${BASE_FILE}-desktop-dev && unlink ${BASE_FILE}-desktop-dev
 
 exit 0
