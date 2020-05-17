fbada5cb1c43adbf1234391bbc333d520939683c
Brendan Forster <github@brendanforster.com>
2020-05-17 14:21:32 -0300

    no need for these steps

--- script/resources/rpm/post.sh
+++ script/resources/rpm/post.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-PROFILE_D_FILE="/etc/profile.d/github-desktop.sh"
 INSTALL_DIR="/usr/lib/github-desktop"
 CLI_DIR="$INSTALL_DIR/resources/app/static"
 
 # add executable permissions for CLI interface
 chmod +x "$CLI_DIR"/github || :
-
-# check if this is a dev install or standard
-if [ -f "$INSTALL_DIR/github-desktop-dev" ]; then
-  BINARY_NAME="github-desktop-dev"
-else
-  BINARY_NAME="github-desktop"
-fi
 
 # create symbolic links to /usr/bin directory
 ln -f -s "$CLI_DIR"/github /usr/bin || :
--- script/resources/rpm/preun.sh
+++ script/resources/rpm/preun.sh
@@ -4,7 +4,5 @@ BASE_FILE="/usr/bin/github"
 
 # remove symbolic links in /usr/bin directory
 test -f ${BASE_FILE} && unlink ${BASE_FILE}
-test -f ${BASE_FILE}-desktop && unlink ${BASE_FILE}-desktop
-test -f ${BASE_FILE}-desktop-dev && unlink ${BASE_FILE}-desktop-dev
 
 exit 0
