3f73a4ce146c0690dedcd352614d2e5b32836a97
Brendan Forster <github@brendanforster.com>
2020-05-17 13:41:53 -0300

    no need to cleanup this profile

--- script/resources/rpm/postun.sh
+++ script/resources/rpm/postun.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-PROFILE_D_FILE="/etc/profile.d/github-desktop.sh"
 BASE_FILE="/usr/bin/github"
-
-# cleanup profile file
-echo "#!/bin/sh" > "${PROFILE_D_FILE}";
-. "${PROFILE_D_FILE}";
-rm "${PROFILE_D_FILE}";
 
 # remove symbolic links in /usr/bin directory
 test -f ${BASE_FILE} && unlink ${BASE_FILE}
