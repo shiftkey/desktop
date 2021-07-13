da9bf483dfc3a8fcae2229fc40d0d4dfcfee670d
Brendan Forster <github@brendanforster.com>
2021-07-13 10:38:08 -0300

    add helpers for flatpak interactions with shells

--- app/src/lib/helpers/linux.ts
+++ app/src/lib/helpers/linux.ts
@@ -1,3 +1,57 @@
+import { join } from 'path'
+import { pathExists as pathExistsInternal } from 'fs-extra'
+import { spawn, SpawnOptionsWithoutStdio } from 'child_process'
+
 export function isFlatpakBuild() {
   return __LINUX__ && process.env.FLATPAK_HOST === '1'
+}
+
+/**
+ * Convert an executable path to be relative to the flatpak host
+ *
+ * @param path a path to an executable relative to the root of the filesystem
+ *
+ */
+function convertToFlatpakPath(path: string) {
+  return join('/var/run/host', path)
+}
+
+/**
+ * Checks the file path on disk exists before attempting to launch a specific shell
+ *
+ * @param path
+ *
+ * @returns `true` if the path can be resolved, or `false` otherwise
+ */
+export async function pathExists(path: string): Promise<boolean> {
+  if (isFlatpakBuild()) {
+    path = convertToFlatpakPath(path)
+  }
+
+  try {
+    return await pathExistsInternal(path)
+  } catch {
+    return false
+  }
+}
+
+/**
+ * Spawn a particular shell in a way that works for Flatpak-based usage
+ *
+ * @param path path to shell, relative to the root of the filesystem
+ * @param args arguments to provide to the shell
+ * @param options additional options to provide to spawn
+ *
+ * @returns a child process to observe and monitor
+ */
+export function spawnShell(
+  path: string,
+  args: string[],
+  options?: SpawnOptionsWithoutStdio
+) {
+  if (isFlatpakBuild()) {
+    return spawn('flatpak-spawn', ['--host', path, ...args], options)
+  }
+
+  return spawn(path, args, options)
 }
