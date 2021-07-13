abc817faaf17b6b04581c57b91ea798de399fe14
Brendan Forster <github@brendanforster.com>
2021-07-13 17:08:41 -0300

    move tests into platform-specific global

--- app/test/unit/helpers/linux-test.ts
+++ app/test/unit/helpers/linux-test.ts
@@ -1,14 +1,16 @@
 import { convertToFlatpakPath } from '../../../src/lib/helpers/linux'
 
-describe('convertToFlatpakPath()', () => {
-  it('converts /usr paths', () => {
-    const path = '/usr/bin/subl'
-    const expectedPath = '/var/run/host/usr/bin/subl'
-    expect(convertToFlatpakPath(path)).toEqual(expectedPath)
-  })
+if (__LINUX__) {
+  describe('convertToFlatpakPath()', () => {
+    it('converts /usr paths', () => {
+      const path = '/usr/bin/subl'
+      const expectedPath = '/var/run/host/usr/bin/subl'
+      expect(convertToFlatpakPath(path)).toEqual(expectedPath)
+    })
 
-  it('preserves /opt paths', () => {
-    const path = '/opt/slickedit-pro2018/bin/vs'
-    expect(convertToFlatpakPath(path)).toEqual(path)
+    it('preserves /opt paths', () => {
+      const path = '/opt/slickedit-pro2018/bin/vs'
+      expect(convertToFlatpakPath(path)).toEqual(path)
+    })
   })
-})
+}
