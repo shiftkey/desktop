d9793bec539f7886a293ff2d25d7638a27a290f7
Brendan Forster <github@brendanforster.com>
2021-07-13 17:20:53 -0300

    add new path to vscodium
    Co-Authored-By: nullrequest <30698906+advaithm@users.noreply.github.com>

--- app/src/lib/editors/linux.ts
+++ app/src/lib/editors/linux.ts
@@ -34,7 +34,11 @@ const editors: ILinuxExternalEditor[] = [
   },
   {
     name: 'VSCodium',
-    paths: ['/usr/bin/codium', '/var/lib/flatpak/app/com.vscodium.codium'],
+    paths: [
+      '/usr/bin/codium',
+      '/var/lib/flatpak/app/com.vscodium.codium',
+      '/usr/share/vscodium-bin/bin/codium',
+    ],
   },
   {
     name: 'Sublime Text',
